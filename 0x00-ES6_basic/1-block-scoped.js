495d16ed79e972c9be46ec715b740dfc2e32fbec
Mshibe700 <philasandeluthuli77@gmail.com>
2024-10-05 04:48:53 +0000

    1. Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,6 @@
-export default function taskBlock(trueOrFalse) {
+export default function taskBlock() {
   const initialTask = false;
   const initialTask2 = true;
-
-  if (trueOrFalse) {
-    const updatedTask = true;
-    const updatedTask2 = false;
-    return [updatedTask, updatedTask2];
-  }
 
   return [initialTask, initialTask2];
 }
