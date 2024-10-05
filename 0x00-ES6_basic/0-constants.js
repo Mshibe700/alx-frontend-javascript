755fa6640a581948976db175ec45c28b89f37105
Mshibe700 <philasandeluthuli77@gmail.com>
2024-10-05 02:07:40 +0000

    0. Const or let?

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,15 +1,15 @@
 export function taskFirst() {
-    const preference = 'I prefer const when I can.';
-    return preference;
-  }
-  
-  export function getLast() {
-    return ' is okay';
-  }
-  
-  export function taskNext() {
-    let description = 'But sometimes let';
-    description += getLast();
-  
-    return description;
-  }
+  const preference = 'I prefer const when I can.';
+  return preference;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  let description = 'But sometimes let';
+  description += getLast();
+
+  return description;
+}
