54d96c2ec6477ff67c25e8aa0928d03aca6d6902
Mshibe700 <philasandeluthuli77@gmail.com>
2024-10-06 07:31:50 +0000

    7. Object property value shorthand syntax

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,9 @@
+export default function createBudgetObject(totalIncome, totalGDP, perCapitaIncome) {
+  const budget = {
+    income: totalIncome,
+    gdp: totalGDP,
+    capita: perCapitaIncome,
+  };
+
+  return budget;
+}
