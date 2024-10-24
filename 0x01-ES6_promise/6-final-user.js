f10a3767d713760be30191bbf2044c16c1bbd7f8
Mshibe700 <philasandeluthuli77@gmail.com>
2024-10-24 13:58:52 +0000

    task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,12 +2,19 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const signUpPromise = signUpUser(firstName, lastName);
-  const uploadPhotoPromise = uploadPhoto(fileName);
-
-  return Promise.allSettled([signUpPromise, uploadPhotoPromise])
-    .then((results) => results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    })));
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((results) => {
+    const processedResults = results.map((item) => {
+      if (item.status === 'rejected') {
+        console.error(`Error: ${item.reason}`);
+      }
+      return {
+        status: item.status,
+        value: item.value || item.reason, // Handles both success and failure
+      };
+    });
+    return processedResults;
+  });
 }
